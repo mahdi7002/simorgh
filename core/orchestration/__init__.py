2a7aaa0a17fe80d6f4e26a5e9b79e8748df6cab7
mahdi7002 <mahdijafarinajafabadi@gmail.com>
2026-09-16 02:21:16 +0330

    feat: export governance contracts

--- core/orchestration/__init__.py
+++ core/orchestration/__init__.py
@@ -2,5 +2,26 @@ from .blackboard import Blackboard
 from .dispatcher import Dispatcher
 from .reviewer import Reviewer
 from .orchestrator import Orchestrator
+from .contracts import (
+    ActionKind,
+    ChangeProposal,
+    Capability,
+    EvidenceArtifact,
+    GateDecision,
+    Governance,
+    RiskLevel,
+)
 
-__all__ = ["Blackboard", "Dispatcher", "Reviewer", "Orchestrator"]
+__all__ = [
+    "Blackboard",
+    "Dispatcher",
+    "Reviewer",
+    "Orchestrator",
+    "ActionKind",
+    "ChangeProposal",
+    "Capability",
+    "EvidenceArtifact",
+    "GateDecision",
+    "Governance",
+    "RiskLevel",
+]
